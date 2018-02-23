5f01cf5dde9a381c1a97621cb9747e8113071c85
Henry Yang <contacthenryyang@gmail.com>
2018-02-23 11:42:56 -0800

    Change file extension

---
+++ client/src/app.jsx
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       data: []
+//     }
+//   }
+
+//   render() {
+//     return(
+//       <h1>Hello, world!</h1>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <App/>, document.getElementById('app')
+// );
+
+// console.log('Hi there');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
