be9c3da3c7e632828bb3e8ac8d578c6c03af62d0
Henry Yang <contacthenryyang@gmail.com>
2018-02-23 16:40:11 -0800

    implement class

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,31 +1,31 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       data: []
-//     }
-//   }
-
-//   render() {
-//     return(
-//       <h1>Hello, world!</h1>
-//     );
-//   }
-// }
-
-// ReactDOM.render(
-//   <App/>, document.getElementById('app')
-// );
-
-// console.log('Hi there');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  render() {
+    return(
+      <h1>Sup, I'm an app</h1>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Sup, I'm an app!</h1>,
-  document.getElementById('app')
+  <App/>, document.getElementById('app')
 );
+
+// console.log('Hi there');
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//   <h1>Sup, I'm an app!</h1>,
+//   document.getElementById('app')
+// );
