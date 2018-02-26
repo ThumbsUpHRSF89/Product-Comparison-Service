959faeac14cfe0fffdb260db7a882bb86bd3d32d
Henry Yang <contacthenryyang@gmail.com>
2018-02-26 00:24:40 -0800

    App dynamically renders dummy data on client side; tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import index from '../client/dist/index.html';
+import Enzyme, { mount, shallow } from 'enzyme';
+import NameRow from '../client/src/nameRow';
+import Adapter from 'enzyme-adapter-react-16';
+import ComparisonTable from '../client/src/comparisonTable';
+Enzyme.configure({adapter: new Adapter() })
 
-test('app renders correctly', () => {
-  
-}
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+
+// describe('NameRow should render correctly', () => {
+//   it('renders name correctly', () => {
+//     const names = ['Mavic Pro'];
+//     const wrapper = mount(
+//       <NameRow name={names} />
+//     );
+//     const p = wrapper.find('.product-name');
+//     expect(p.text()).toBe('Mavic Pro');
+//   })
+// })
+
+// describe('App should render correctly', () => {
+//   it ('renders correctly', () => {
+//     const
+//   })
+// })
+
+test('ComparisonTable should render a table', () => {
+  const wrapper = shallow(<ComparisonTable data={[]}/>);
+  expect(wrapper.is('table')).toEqual(true);
+});
