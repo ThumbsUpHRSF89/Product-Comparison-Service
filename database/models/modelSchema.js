cffba10dc50ab50f7deec725e1841ad0e1968d47
Henry Yang <contacthenryyang@gmail.com>
2018-02-28 02:20:54 -0800

    Fix bug- name shouldn't be unique

--- database/models/modelSchema.js
+++ database/models/modelSchema.js
@@ -8,7 +8,6 @@ const productSchema = mongoose.Schema({
   },
   name: {
     type: String,
-    unique: true,
   },
   price: String,
   weight: String,
@@ -18,7 +17,7 @@ const productSchema = mongoose.Schema({
   image_url: String,
   overallReview: Number,
   reviewNumber: Number,
-})
+});
 
 let ProductModel = mongoose.model('Product', productSchema);
 
