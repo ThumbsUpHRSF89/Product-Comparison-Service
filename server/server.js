43c66d8e1606c39af1f2134c02c39ccac0057544
Henry Yang <contacthenryyang@gmail.com>
2018-02-28 16:00:02 -0800

    Implement server delivers docs of number + 4 of the same category

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const controller = require('../database/controllers/controller.js')
-const models = require('../database/models/modelSchema.js')
+const controller = require('../database/controllers/controller.js');
+const models = require('../database/models/modelSchema.js');
 
-
-app.use(express.static(path.join(__dirname, '/../client/dist')))
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 // app.get('/:Id', (req, res) => {
 //   console.log('Id = ', req.params.Id)
@@ -15,17 +14,26 @@ app.use(express.static(path.join(__dirname, '/../client/dist')))
 
 app.use('/:Id', (req, res) => {
   console.log('req.params.Id = ', req.params.Id);
-  let productId = parseInt(req.params.Id)
+  let productId = parseInt(req.params.Id);
   let dataForServer = [];
   controller.findOne(productId, (err, doc) => {
     if (err) {
       console.log('Database error: ', err);
     } else {
-      console.log('Doc = ', doc)
       dataForServer.push(doc);
       let category = doc.category;
-      console.log('category = ', category);
-      res.send(category);
+      controller.findFour(category, (err, docs) => {
+        if (err) {
+          console.log('findFour error: ', err);
+        } else {
+          console.log(docs);
+          docs.forEach(object => {
+            dataForServer.push(object);
+            console.log('dataForServer = ', dataForServer);
+          });
+          res.send(dataForServer);
+        }
+      });
     }
   });
   // models.ProductModel.findOne({id: productId}, (err , doc) => {
@@ -37,7 +45,7 @@ app.use('/:Id', (req, res) => {
   // })
 });
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log('Server listening on port 8000');
