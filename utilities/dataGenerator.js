39c19c1e52819335c0a647e6ad5ac50b0d22801b
Henry Yang <contacthenryyang@gmail.com>
2018-03-05 16:35:22 -0800

    Replace marlin's image sources

--- utilities/dataGenerator.js
+++ utilities/dataGenerator.js
@@ -66,11 +66,28 @@ var phoneDecorator = (someArr) => {
 
 var tvDecorator = (someArr) => {
   var dataStorage = [];
+  var tvImages = [
+    'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5748/5748203_rd.jpg',
+    'http://images.samsung.com/is/image/samsung/my-hd-j4303-ua32j4303akxxm-002-front-indigoblue?scl=1&fmt=png-alpha',
+    'https://www.thegoodguys.com.au/wcsstore/TGGCAS/idcplg?IdcService=GET_FILE&RevisionSelectionMethod=LatestReleased&noSaveAs=1&dDocName=50046771_506407&Rendition=ZOOMIMAGE',
+    'https://i5.walmartimages.ca/images/Enlarge/747/_01/999999-887276046747_01.jpg',
+    'http://www.220-electronics.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung-ps-50c450-plasma-multi-system-tv.jpg',
+    'https://images-na.ssl-images-amazon.com/images/I/81m9jhinCmL._SL1500_.jpg',
+    'http://eventsbyfabulous.com/wp-content/uploads/2013/09/samsung-tv-front.jpg',
+    'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5844/5844904_sd.jpg',
+    'http://cdn2.expertreviews.co.uk/sites/expertreviews/files/2017/08/best_tv_deals_-_lg_22mt49df.jpg?itok=rO2_w58f',
+    'https://images.philips.com/is/image/PhilipsConsumer/40HFL3011T_12-IMS-en_GB?&wid=960&hei=960&$jpglarge$',
+    'http://nebula.wsimg.com/9fe26742465719fe398d51b310b766ae?AccessKeyId=261E41F217B038BD12B5&disposition=0&alloworigin=1',
+    'https://www.thegoodguys.com.au/wcsstore/TGGCAS/idcplg?IdcService=GET_FILE&RevisionSelectionMethod=LatestReleased&noSaveAs=1&dDocName=50044402_483423&Rendition=ZOOMIMAGE',
+    'https://cdn2.macworld.co.uk/cmsdata/features/3664165/samsung-smart-tv.jpg',
+    'https://brain-images-ssl.cdn.dixons.com/7/7/10149777/u_10149777.jpg',
+  ];
   for (var i = 0; i < 25; i++) {
     someArr[i].price = ('$' + randomNumberGen(2000) + '.00');
     someArr[i].weight = randomNumberGen(50) + ' lb',
     someArr[i].dimensions = randomNumberGen(80) + ' x ' + (randomNumberGen(10)) + ' x ' + randomNumberGen(40) + ' in';
     someArr[i].color = color[randomNumberGen(color.length)],
+    someArr[i].image_url = tvImages[randomNumberGen(tvImages.length)],
     someArr[i].overallReview = randomNumberGen(6),
     someArr[i].reviewNumber = randomNumberGen(400)
     dataStorage.push(someArr[i]);
