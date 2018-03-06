79b9003111caceff7cd86c65c7dec772556c2cc0
Henry Yang <contacthenryyang@gmail.com>
2018-03-05 17:31:19 -0800

    Edit html tags

--- client/src/headerRow.jsx
+++ client/src/headerRow.jsx
@@ -6,15 +6,15 @@ function HeaderRow(props) {
     <tr id='header-row'>
       <td id="empty-first-column" width="250px" />
       {props.data.map(object =>
-        <td width='250px'>
+        <th width='250px'>
           <a href={`http://localhost:8000/${object.id}`}>
             <div>
               <img src={object.image_url} height="250px" width="250px" />
             </div>
-            <th className='product-name' width='250px'>{object.name}</th>
+            <div className='product-name' width='250px'>{object.name}</div>
           </a>
           <button className='purchase-button' width="250px">Add to Cart</button>
-        </td>
+        </th>
       )}
     </tr>
   );
