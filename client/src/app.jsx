fac98e2ee1693d22ac82a13e28988644698e0e98
Henry Yang <contacthenryyang@gmail.com>
2018-03-08 11:43:29 -0800

    Import in CSS so it'll be packaged with webpack's bundle.js

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ComparisonTable from './comparisonTable.jsx';
-
+import CSS from '../dist/styles.css';
+// const CSS = require('../dist/styles.css');
 
 function App () {
   return(
