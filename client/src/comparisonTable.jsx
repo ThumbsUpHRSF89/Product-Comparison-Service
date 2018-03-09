0d3b6e9d88f22efa03889170b7586f36eae8b91f
Henry Yang <contacthenryyang@gmail.com>
2018-03-09 15:48:26 -0800

    Remove dummy data and add css tags

--- client/src/comparisonTable.jsx
+++ client/src/comparisonTable.jsx
@@ -7,78 +7,7 @@ class ComparisonTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-        {
-          id: 50,
-          name: 'Mavic Pro',
-          price: 525,
-          weight: 3,
-          manufacturer: 'Ambarella',
-          dimensions: '4 x 9 x 6 inches',
-          color: 'Vermillion',
-          image_url:
-            'https://truimg.toysrus.com/product/images/sharper-image-rechargeable-dx-2-stunt-drone-2.4-ghz-black--54BCFCEC.zoom.jpg?fit=inside|485:485',
-          category: 'drone',
-          overallReview: 5,
-          reviewNumber: 365,
-        },
-        {
-          id: 51,
-          name: 'Hornet',
-          price: 566,
-          weight: 6,
-          manufacturer: 'Parrot SA',
-          dimensions: '7 x 10 x 7 inches',
-          color: 'Vermillion',
-          image_url:
-            'https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/images/2017/08/drone-02.jpg?itok=ZcNajxlf&fc=50,50',
-          category: 'drone',
-          overallReview: 3,
-          reviewNumber: 256,
-        },
-        {
-          id: 52,
-          name: 'Titanic',
-          price: 999,
-          weight: 2,
-          manufacturer: 'Dajiang Innovations',
-          dimensions: '8 x 7 x 7 inches',
-          color: 'White',
-          image_url:
-            'https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/images/2017/08/drone-08.jpg?itok=dEigMkKk&fc=50,50',
-          category: 'drone',
-          overallReview: 1,
-          reviewNumber: 81,
-        },
-        {
-          id: 53,
-          name: 'Spark',
-          price: 1018,
-          weight: 2,
-          manufacturer: 'Ambarella',
-          dimensions: '17 x 8 x 6 inches',
-          color: 'White',
-          image_url:
-            'https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/images/2017/08/drone-07.jpg?itok=dEigMkKk&fc=50,50',
-          category: 'drone',
-          overallReview: 5,
-          reviewNumber: 259,
-        },
-        {
-          id: 54,
-          name: 'Eagle',
-          price: 639,
-          weight: 2,
-          manufacturer: 'GoPro',
-          dimensions: '15 x 8 x 7 inches',
-          color: 'White',
-          image_url:
-            'https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/images/2017/08/drone-02.jpg?itok=ZcNajxlf&fc=50,50',
-          category: 'drone',
-          overallReview: 1,
-          reviewNumber: 339,
-        },
-      ],
+      data: [],
       attributes: [],
     };
   }
@@ -137,12 +66,12 @@ class ComparisonTable extends React.Component {
 
   render() {
     return (
-      <table>
+      <table id="table">
         <tbody className="tables">
           <HeaderRow data={this.state.data} />
           {this.state.attributes.map(property => (
             <tr id={property}>
-              <td>{this.englishCasing(property)}</td>
+              <td className="object-property-cell">{this.englishCasing(property)}</td>
               {this.state.data.map((object, index) =>
                 <td className={`object-${index}`}>{object[property]}</td>)}
             </tr>
