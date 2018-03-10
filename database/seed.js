59df4f8a0f43c19877ef92abe006ac1bf903377d
Henry Yang <contacthenryyang@gmail.com>
2018-03-10 15:31:56 -0800

    Service will disconnect when done seeding

--- database/seed.js
+++ database/seed.js
@@ -1,15 +1,16 @@
-var data = require('../dummyData/aggregatedData');
-var mongoose = require('mongoose');
-var Product = require('./models/modelSchema');
+var data = require("../dummyData/aggregatedData");
+var mongoose = require("mongoose");
+var Product = require("./models/modelSchema");
 
-mongoose.connect('mongodb://localhost/hackazon');
+mongoose.connect("mongodb://localhost/hackazon");
 
 let insertOne = (item, callback) => {
   Product.ProductModel.create(item, callback);
-}
+};
 
 var seedDb = function(data) {
-  // your code here!
+  let insertionCount = 0;
+  let insertionTarget = data.length;
   data.forEach(element => {
     let newDoc = new Product.ProductModel({
       id: element.id,
@@ -21,18 +22,23 @@ var seedDb = function(data) {
       category: element.category,
       image_url: element.image_url,
       overallReview: element.overallReview,
-      numberOfRatings: element.numberOfRatings,
+      numberOfRatings: element.numberOfRatings
     });
     // console.log('newDoc = ', newDoc);
-    insertOne(newDoc, (err) => {
+    insertOne(newDoc, err => {
       if (err) {
-        console.log('Doc save error', err);
+        console.log("Doc save error", err);
       } else {
-        console.log('Doc saved!', element.id);
+        console.log("Doc saved!", element.id);
+        insertionCount += 1;
+        if (insertionCount === insertionTarget) {
+          console.log('disconnecting database');
+          mongoose.disconnect();
+        }
       }
-    })
-  })
+    });
+  });
 };
 
-seedDb(data.aggregate);
 
+seedDb(data.aggregate);
