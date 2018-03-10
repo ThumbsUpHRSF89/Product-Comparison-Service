1f3e898260284a9623475e88b1caf8e867bf179d
Henry Yang <contacthenryyang@gmail.com>
2018-03-10 13:31:12 -0800

    Data returned to server will have no repeats

--- utilities/chooseRandomFour.js
+++ utilities/chooseRandomFour.js
@@ -2,12 +2,12 @@ let chooseRandomFour = (arrData, targetArr) => {
   let randomIndexGenerator = length => {
     return Math.floor(Math.random() * length);
   };
+  let listOfIndices = [targetArr[0].id];
   while (targetArr.length < 5) {
+    let randomIndex = randomIndexGenerator(arrData.length);
     // if doc is not present in targetArr
-    if (
-      targetArr.indexOf(arrData[randomIndexGenerator(arrData.length)]) === -1
-    ) {
-      targetArr.push(arrData[randomIndexGenerator(arrData.length)]);
+    if (targetArr.indexOf(arrData[randomIndex]) === -1) {
+        targetArr.push(arrData[randomIndex]);
     }
   }
 };
